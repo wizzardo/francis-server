cf279e968276ac62c764350ec534c1e2abf8de54
wizzardo <wizzardobox@gmail.com>
2017-02-08 22:41:16 +0100

    added simple DBService

---
+++ src/main/java/com/wizzardo/francis/services/DBService.java
@@ -0,0 +1,145 @@
+package com.wizzardo.francis.services;
+
+import com.wizzardo.http.framework.di.PostConstruct;
+import com.wizzardo.http.framework.di.Service;
+import com.wizzardo.tools.collections.flow.Flow;
+import com.wizzardo.tools.collections.flow.FlowProcessor;
+import com.wizzardo.tools.interfaces.Mapper;
+import com.wizzardo.tools.io.IOTools;
+import com.wizzardo.tools.misc.Unchecked;
+import com.wizzardo.tools.misc.pool.Pool;
+import com.wizzardo.tools.misc.pool.PoolBuilder;
+import com.wizzardo.tools.misc.pool.SimpleHolder;
+
+import java.io.IOException;
+import java.sql.*;
+
+/**
+ * Created by wizzardo on 24/01/17.
+ */
+public class DBService implements Service, PostConstruct {
+
+    Pool<Connection> connectionPool = new PoolBuilder<Connection>()
+            .holder((pool, value) -> new SimpleHolder<>(pool, value))
+            .supplier(this::createConnection)
+            .queue(PoolBuilder.createSharedQueueSupplier())
+            .limitSize(16)
+            .build();
+
+    @Override
+    public void init() {
+        Unchecked.call(() -> Class.forName("org.postgresql.Driver"));
+    }
+
+    public <R> R provide(Pool.UnsafeMapper<Connection, R> mapper) {
+        return connectionPool.provide(mapper);
+    }
+
+    private Connection createConnection() {
+        return Unchecked.call(() -> {
+            String url = "jdbc:postgresql://localhost/francis";
+            return DriverManager.getConnection(url, "username", "password");
+        });
+    }
+
+    static class FlowSql extends Flow<ResultSet> {
+
+        boolean stop = false;
+
+        public static Flow<ResultSet> of(ResultSet rs) {
+            return new FlowSql(rs);
+        }
+
+        @Override
+        protected void start() {
+            if (child == null)
+                return;
+
+            process();
+
+            onEnd();
+        }
+
+        final ResultSet resultSet;
+
+        public FlowSql(ResultSet resultSet) {
+            this.resultSet = resultSet;
+        }
+
+        protected void process() {
+            FlowProcessor<ResultSet, ?> child = this.child;
+
+            try (ResultSet rs = this.resultSet;) {
+                while (rs.next()) {
+                    if (stop)
+                        break;
+
+                    child.process(rs);
+                }
+            } catch (SQLException e) {
+                throw Unchecked.rethrow(e);
+            }
+        }
+    }
+
+    public <R> R executeQuery(String sql, Mapper<Flow<ResultSet>, R> mapper) {
+        return provide(connection -> mapper.map(FlowSql.of(connection.createStatement().executeQuery(sql))));
+    }
+
+    public <R> R executeQuery(String sql, Object[] args, Mapper<Flow<ResultSet>, R> mapper) {
+        return provide(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            return mapper.map(FlowSql.of(statement.executeQuery()));
+        });
+    }
+
+    public int executeUpdate(String sql, Object[] args) {
+        return provide(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            return statement.executeUpdate();
+        });
+    }
+
+    public <R> R execute(String sql, Object[] args, Mapper<Flow<ResultSet>, R> mapper) {
+        return provide(connection -> {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            boolean execute = statement.execute();
+            if (execute)
+                return mapper.map(FlowSql.of(statement.getResultSet()));
+            else
+                throw new RuntimeException("Expected ResultSet");
+        });
+    }
+
+    public void execute(String sql) {
+        provide(connection -> connection.createStatement().execute(sql));
+    }
+
+    public static Object[] args(Object... args) {
+        return args;
+    }
+
+    public static void main(String[] args) throws IOException {
+        DBService dbService = new DBService();
+        dbService.init();
+
+        DataService dataService = new DataService();
+        dataService.dbService = dbService;
+
+        String scheme = new String(IOTools.bytes(DBService.class.getResourceAsStream("/Schema.sql")));
+        System.out.println("applying scheme:");
+        for (String s : scheme.split(";")) {
+            System.out.println(s);
+            dbService.execute(s);
+        }
+    }
+}
---
+++ src/main/resources/Schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS application;
+CREATE TABLE application (
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(128)
+);
+ALTER TABLE application
+  ADD UNIQUE (name);
+
+DROP TABLE IF EXISTS instance;
+CREATE TABLE instance (
+  id             SERIAL PRIMARY KEY,
+  application_id INT,
+  version        VARCHAR(64),
+  ip             VARCHAR(16),
+  mac            CHAR(17),
+  hostname       VARCHAR(128)
+);
+
+DROP TABLE IF EXISTS transformation;
+CREATE TABLE transformation (
+  id             SERIAL PRIMARY KEY,
+  application_id INT,
+  before         VARCHAR(1024),
+  after          VARCHAR(1024),
+  variables      VARCHAR(1024)
+);
