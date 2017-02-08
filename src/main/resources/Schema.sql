d084af15cfb96e5a6e3a2ac75313f6be2a330ea2
wizzardo <wizzardobox@gmail.com>
2017-02-08 22:51:52 +0100

    minor improvements

--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -15,11 +15,16 @@ CREATE TABLE instance (
   mac            CHAR(17),
   hostname       VARCHAR(128)
 );
+ALTER TABLE instance
+  ADD UNIQUE (application_id, mac);
 
 DROP TABLE IF EXISTS transformation;
 CREATE TABLE transformation (
   id             SERIAL PRIMARY KEY,
   application_id INT,
+  version        INT,
+  last_updated   TIMESTAMP,
+  date_created   TIMESTAMP,
   before         VARCHAR(1024),
   after          VARCHAR(1024),
   variables      VARCHAR(1024)
