bbcef899504e6298cd25dcc8bcb0c91a1c523e77
wizzardo <wizzardobox@gmail.com>
2017-02-09 23:29:26 +0100

    minor improvements

--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -20,12 +20,15 @@ ALTER TABLE instance
 
 DROP TABLE IF EXISTS transformation;
 CREATE TABLE transformation (
-  id             SERIAL PRIMARY KEY,
-  application_id INT,
-  version        INT,
-  last_updated   TIMESTAMP,
-  date_created   TIMESTAMP,
-  before         VARCHAR(1024),
-  after          VARCHAR(1024),
-  variables      VARCHAR(1024)
+  id               SERIAL PRIMARY KEY,
+  application_id   INT,
+  version          INT,
+  last_updated     TIMESTAMP,
+  date_created     TIMESTAMP,
+  class_name       VARCHAR(256),
+  method           VARCHAR(128),
+  method_descriptor VARCHAR(512),
+  before           VARCHAR(1024),
+  after            VARCHAR(1024),
+  variables        VARCHAR(1024)
 );
